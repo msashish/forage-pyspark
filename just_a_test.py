24011ca60807121051c6ff6627ed713062631ec6
Ashish Sheelavantar <58141385+msashish@users.noreply.github.com>
2021-06-13 14:55:54 +0530

    Update just_a_test.py

--- just_a_test.py
+++ just_a_test.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("excel-email-pipeline").getOrCreate()
 
-df = spark.read.format("com.crealytics.spark.excel").option("Header", "true").option("inferSchema", "true").load("ANZ_synthesised_transaction_dataset.xlsx")
+df = spark.read.format("com.crealytics.spark.excel").option("Header", "true").option("inferSchema", "true").load("ABC_synthesised_transaction_dataset.xlsx")
 # class 'pyspark.sql.dataframe.DataFrame'
 df.show(2, truncate=False)
 print(df.printSchema())
